b0265afcf46c96ca363d772a4da01d36dee71ca8
almina-mac <alm.0554.mina@gmail.com>
2019-07-26 02:02:43 +0900

    Update: add main scheduler

--- scheduler.py
+++ scheduler.py
@@ -19,28 +19,11 @@ def job_everyday():
 
 
 def main():
-    # schedule.every().sunday.do(job_everyweek)
-    # schedule.every().day.at("16:00").do(job_everyday)
+    schedule.every().sunday.do(job_everyweek)
+    schedule.every().day.at("16:00").do(job_everyday)
 
-    schedule.every(1).minutes.do(job_everyweek)
+    # schedule.every(1).minutes.do(job_everyweek)
     # schedule.every(1).minutes.do(job_everyday)
-
-
-    # #10分毎にjobを実行
-    # schedule.every(1).minutes.do(job1)
-    # schedule.every(2).minutes.do(job2)
-
-    # #毎時間ごとにjobを実行
-    # schedule.every().hour.do(job)
-
-    # #AM10:30にjobを実行
-    # schedule.every().day.at("10:30").do(job)
-
-    # #月曜日にjobを実行
-    # schedule.every().monday.do(job)
-
-    # #水曜日の13:15にjobを実行
-    # schedule.every().wednesday.at("13:15").do(job)
 
     while True:
         schedule.run_pending()
