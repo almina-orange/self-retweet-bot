8ad7c7a86c866f67a3a631b5df39a0194bd1060a
almina-mac <alm.0554.mina@gmail.com>
2019-07-27 14:56:45 +0900

    Update: change to use APScheduler and run as process

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,5 @@
 #coding:UTF-8
-import schedule
+from apscheduler.schedulers.blocking import BlockingScheduler
 import time
 
 import get_all_tweets
@@ -9,7 +9,7 @@ import post_self_retweet
 from LINE_notify import post_line_notify
 
 
-def job_everyweek():
+def week_job():
 
     get_all_tweets.main()
     get_works.main()
@@ -18,7 +18,7 @@ def job_everyweek():
     post_line_notify('everyweek')
 
 
-def job_everyday():
+def day_job():
 
     post_self_retweet.main()
 
@@ -26,29 +26,19 @@ def job_everyday():
     post_line_notify('everyday')
 
 
-def job_everyhour():
+def hours_job():
 
     ### Post to LINE-Notify
     post_line_notify('everyhour')
 
 
-def job_everyminute():
-
-    ### Post to LINE-Notify
-    post_line_notify('everyminute')
-
-
 def main():
-    schedule.every().sunday.do(job_everyweek)
-    schedule.every().day.at("07:00").do(job_everyday)
-    schedule.every().hour.do(job_everyhour)
 
-    # schedule.every(1).minutes.do(job_everyweek)
-    # schedule.every(1).minutes.do(job_everyday)
+    sched = BlockingScheduler()
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    sched.add_job(hours_job, 'interval', hours=1)
+    sched.add_job(day_job, 'cron', hour=7)
+    sched.add_job(week_job, 'cron', day_of_week='sun')
 
 
 if __name__ == "__main__":
